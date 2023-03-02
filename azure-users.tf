79bfdb9f2e76555545604b9fe39370d241bb909b
Rui Lopes <rgl@ruilopes.com>
2023-03-02 12:52:54 +0000

    use the square brackets syntax to access a list item

--- azure-users.tf
+++ azure-users.tf
@@ -22,8 +22,8 @@ resource "azuread_user" "alice" {
   surname             = "Doe"
   display_name        = "Alice Doe"
   mail_nickname       = "${var.prefix}-alice.doe"
-  mail                = "${var.prefix}-alice.doe@${data.azuread_domains.current.domains.0.domain_name}"
-  user_principal_name = "${var.prefix}-alice.doe@${data.azuread_domains.current.domains.0.domain_name}"
+  mail                = "${var.prefix}-alice.doe@${data.azuread_domains.current.domains[0].domain_name}"
+  user_principal_name = "${var.prefix}-alice.doe@${data.azuread_domains.current.domains[0].domain_name}"
   usage_location      = "PT"
   password            = random_password.alice.result
 }
@@ -34,8 +34,8 @@ resource "azuread_user" "bob" {
   surname             = "Doe"
   display_name        = "Bob Doe"
   mail_nickname       = "${var.prefix}-bob.doe"
-  mail                = "${var.prefix}-bob.doe@${data.azuread_domains.current.domains.0.domain_name}"
-  user_principal_name = "${var.prefix}-bob.doe@${data.azuread_domains.current.domains.0.domain_name}"
+  mail                = "${var.prefix}-bob.doe@${data.azuread_domains.current.domains[0].domain_name}"
+  user_principal_name = "${var.prefix}-bob.doe@${data.azuread_domains.current.domains[0].domain_name}"
   usage_location      = "PT"
   password            = random_password.bob.result
 }
